383ff60fe8e71e7408bfd672f278faa7341f5710
UmmuhanTeke <ummuhantekeee@gmail.com>
2024-09-03 23:52:17 +0300

    Ümmühan Teke değişikliği tamamladı, kolay gelsin.

---
+++ src/SecondPrac/Ummuhan/Odev1.java
@@ -0,0 +1,54 @@
+package SecondPrac.Ummuhan;
+
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+    public class Odev1 extends BaseDriver {
+
+        @Test
+        public void Test1(){
+            //  1) Bu siteye gidin. -> http://demoqa.com/text-box
+            //  2) Full Name kısmına "Automation" girin.
+            //  3) Email kısmına "Testing@gmail.com" girin.
+            //  4) Current Address kısmına "Testing Current Address" girin.
+            //  5) Permanent Address kısmına "Testing Permanent Address" girin.
+            //  6) Submit butonuna tıklayınız.
+            //  7) Full Name'in, "Automation"ı içinde bulundurduğunu doğrulayın.
+            //  8) Email'in, "Testing"ı içinde bulundurduğunu doğrulayın.
+
+            driver.get("https://demoqa.com/text-box");
+
+            String fullName = "Ümmühan Teke";
+            WebElement fullnameBox = driver.findElement(By.cssSelector("div[id='userName-wrapper'] input"));
+            fullnameBox.sendKeys(fullName);
+            MyFunc.Wait(2);
+
+            String emailMe = "ummuhantekeee@gmail.com";
+            WebElement email = driver.findElement(By.cssSelector("div[id='userEmail-wrapper'] input"));
+            email.sendKeys(emailMe);
+            MyFunc.Wait(2);
+
+            String address = "Mimar Sinan Mahallesi / Aydın ";
+            WebElement currentAddress = driver.findElement(By.cssSelector("div[id='currentAddress-wrapper'] textarea"));
+            currentAddress.sendKeys(address);
+            MyFunc.Wait(2);
+
+            String pAddress = "Erenler Mahallesi / İstanbul ";
+            WebElement permanentAddress = driver.findElement(By.cssSelector("div[id='permanentAddress-wrapper'] textarea"));
+            permanentAddress.sendKeys(pAddress);
+            MyFunc.Wait(2);
+
+            WebElement buttonSubmit = driver.findElement(By.cssSelector("div[class='mt-2 justify-content-end row'] button"));
+            buttonSubmit.click();
+            MyFunc.Wait(2);
+
+            BekleKapat();
+
+
+        }
+
+    }
+}
