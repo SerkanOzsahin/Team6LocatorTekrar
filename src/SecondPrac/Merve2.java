7b2e5bfd8c3d1397106d4e8ba412088c8156ee80
kayyamervee <kayaamervee01@gmail.com>
2024-09-04 01:56:04 +0300

    Test2 bitmiştir.

---
+++ src/SecondPrac/Merve2.java
@@ -0,0 +1,39 @@
+package SecondPrac;
+import Utility.BaseDriver;
+import Utility.MyFunc;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Merve2 extends BaseDriver {
+
+    @Test
+    public void Test(){
+        driver.get("https://demo.applitools.com/");
+        MyFunc.Wait(2);
+
+        String fullname="ttechno@gmail.com";
+        WebElement fullnameBox=driver.findElement(By.cssSelector("div[class='form-group']>:nth-child(2)"));
+        fullnameBox.sendKeys(fullname);
+        MyFunc.Wait(2);
+
+        String password="techno123.";
+        WebElement passwordBox=driver.findElement(By.cssSelector("div[class='form-group'] input[type='password']"));
+        passwordBox.sendKeys(password);
+        MyFunc.Wait(2);
+
+        WebElement signIn=driver.findElement(By.cssSelector("div[class='buttons-w'] a"));
+        signIn.click();
+        MyFunc.Wait(2);
+
+        WebElement kontrolText=driver.findElement(By.cssSelector("h6[style='text-align: center;font-size: x-large;color:red']"));
+        Assert.assertTrue("Beklenen yazıya ulaşılamadı.",kontrolText.getText().equals("Your nearest branch closes in: 30m 5s"));
+        MyFunc.Wait(2);
+        BekleKapat();
+
+
+
+
+    }
+}
